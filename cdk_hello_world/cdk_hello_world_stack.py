e25f24ff1b535c7cdbd60c39171a3833e4044eab
Harrison <captainj777@gmail.com>
2024-10-06 15:30:00 -0400

    Hello World Stack creation"

--- cdk_hello_world/cdk_hello_world_stack.py
+++ cdk_hello_world/cdk_hello_world_stack.py
@@ -1,19 +1,61 @@
 from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+  Stack,
+  aws_lambda as _lambda, # Import the Lambda module
+  CfnOutput # Import CfnOutput
 )
 from constructs import Construct
 
 class CdkHelloWorldStack(Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
-        super().__init__(scope, construct_id, **kwargs)
+  def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+    # Define the Lambda function resource
+    my_function = _lambda.Function(
+      self, "HelloWorldFunction", 
+      runtime = _lambda.Runtime.NODEJS_20_X, # Provide any supported Node.js runtime
+      handler = "index.handler",
+      code = _lambda.Code.from_inline(
+        """
+        exports.handler = async function(event) {
+          return {
+            statusCode: 200,
+            body: JSON.stringify('Hello World!'),
+          };
+        };
+        """
+      ),
+    )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkHelloWorldQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+    # Define the Lambda function URL resource
+    my_function_url = my_function.add_function_url(
+      auth_type = _lambda.FunctionUrlAuthType.NONE,
+    )
+
+    #Define a CFN output for the function URL
+    CfnOutput(self, "myFunctionUrlOutput", value=my_function_url.url)
+
+
+
+
+
+
+# from aws_cdk import (
+#     # Duration,
+#     Stack,
+#     # aws_sqs as sqs,
+# )
+# from constructs import Construct
+
+# class CdkHelloWorldStack(Stack):
+
+#     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+#         super().__init__(scope, construct_id, **kwargs)
+
+#         # The code that defines your stack goes here
+
+#         # example resource
+#         # queue = sqs.Queue(
+#         #     self, "CdkHelloWorldQueue",
+#         #     visibility_timeout=Duration.seconds(300),
+#         # )
